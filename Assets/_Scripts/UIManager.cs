f1c3fa0c0c2dc68b8927a1f739809509142c509b
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2023-12-18 10:53:03 +0700

    bug

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -16,6 +16,8 @@ public class UIManager : MonoBehaviour
     public ParticleSystem red;
     public ParticleSystem finish1;
     public ParticleSystem finish2;
+    public GameObject giveupBtn;
+    public GameObject rondePanel;
 
     private void Start()
     {
@@ -29,6 +31,8 @@ public class UIManager : MonoBehaviour
     {
         gameOverPanel.SetActive(true);
         UpdateGameOverText();
+        giveupBtn.gameObject.SetActive(false);
+        rondePanel.gameObject.SetActive(false);
     }
 
     private void UpdateGameOverText()
@@ -39,11 +43,15 @@ public class UIManager : MonoBehaviour
 
     private void HideGameOverPanel()
     {
+        giveupBtn.gameObject.SetActive(true);
+        rondePanel.gameObject.SetActive(true);
         gameOverPanel.SetActive(false);
     }
 
     internal void ShowRoundOverPanel()
     {
+        giveupBtn.gameObject.SetActive(false);
+        rondePanel.gameObject.SetActive(false);
         hideText();
         roundOverPanel.SetActive(true);
         UpdateRoundOverText();
